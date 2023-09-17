946bf53dc9d6d3001bcc247a027d0e63bfde152b
YtBelll <ytbell@outlook.com>
2023-09-16 23:37:07 -0300

    comentarios

--- pagina/script.js
+++ pagina/script.js
@@ -1,8 +1,8 @@
-/*n sei configurar isso*/
+/*receber objetos*/
 var cpf = document.getElementById("cpf")
 var senha = document.getElementById("senha")
 
-entrar(){ /**testes q falharam */
+entrar(){ /**testes para o login*/
     if(cpf.textContent=="12312312312"||senha.textContent=="123456"){
         alert("login realizado com sucesso");
     } else{
@@ -12,7 +12,7 @@ entrar(){ /**testes q falharam */
 
 
 /*
-entrar() {mandar os dados para fachada
+entrar() {mandar os dados para fachada algum dia
     cpf.textContent;
     senha.textContent;
 };
